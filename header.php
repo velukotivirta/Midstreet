2374ac272b114e640f846e872f7005ae3a5f19e9
Vertti Koivula <vertti.koivula@gmail.com>
2025-05-21 15:11:19 +0300

    Added logo

--- header.php
+++ header.php
@@ -1,8 +1,33 @@
 <header class="header">
+    <div class="logo">
+      <a href="#" class="logo-link">
+        <svg class="svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 1024" fill="none">
+          <rect fill="none"/>
+          <line x1="170" y1="461.5" x2="670" y2="461.5" stroke="white"/>
+          <line x1="770" y1="461.5" x2="1270" y2="461.5" stroke="white"/>
+          <line x1="170" y1="561.5" x2="670" y2="561.5" stroke="white"/>
+          <line x1="770" y1="561.5" x2="1270" y2="561.5" stroke="white"/>
+          <line x1="669.502" y1="563.68" x2="689.25" y2="337.956" stroke="white"/>
+          <line y1="-0.5" x2="226.586" y2="-0.5" transform="matrix(0.0871557 0.996195 0.996195 -0.0871557 750 338)" stroke="white"/>
+          <line x1="720.5" y1="338" x2="720.5" y2="388" stroke="white"/>
+          <line x1="720.5" y1="412" x2="720.5" y2="462" stroke="white"/>
+          <line x1="720.5" y1="487" x2="720.5" y2="537" stroke="white"/>
+        </svg>
+      </a>
+    </div>
     <nav>
-      <div class="naviBar">
-      <div class="midstreetLogo"><p>Testi</p>
-      </div>
-      </div>
+      <ul class="nav-list">
+        <li class="nav-list-item">
+        </li>
+        <li class="nav-list-item">
+          <a href="#" class="nav-link">About</a>
+        </li>
+        <!--<li class="nav-list-item">
+            <a href="#" class="nav-link">Sign up</a> Not for MVP -->
+        </li>
+        <!-- <li class="nav-list-item">
+            <a href="#" class="nav-link">Login</a> Not for MVP -->
+        </li>
+      </ul>
     </nav>
   </header>
