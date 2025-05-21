5661eb77c4782be6b92e743ae29ca725deecb5fc
Vertti Koivula <vertti.koivula@gmail.com>
2025-05-21 09:27:07 +0300

    Added header, added logo

--- header.php
+++ header.php
@@ -1,23 +1,8 @@
 <header class="header">
-    <div class="logo">
-      <a href="./" class="logo-link">
-        <p class="midstreet-logo">Midstreet</p>
-      </a>
-    </div>
     <nav>
-      <ul class="nav-list">
-        <li class="nav-list-item">
-          <a href="yhteystiedot.php" class="nav-link">Yhteystiedot</a>
-        </li>
-        <li class="nav-list-item">
-          <a href="#" class="nav-link">About</a>
-        </li>
-        <!--<li class="nav-list-item">
-            <a href="#" class="nav-link">Sign up</a> Not for MVP -->
-        </li>
-        <!-- <li class="nav-list-item">
-            <a href="#" class="nav-link">Login</a> Not for MVP -->
-        </li>
-      </ul>
+      <div class="naviBar">
+      <div class="midstreetLogo"><p>Testi</p>
+      </div>
+      </div>
     </nav>
   </header>
