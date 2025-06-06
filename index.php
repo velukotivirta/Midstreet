c3744788d9e85133346d762975ba762618eea668
Vertti Koivula <vertti.koivula@gmail.com>
2025-06-06 14:21:02 +0300

    Fixes:
    Header now working properly
    Added pictures

--- index.php
+++ index.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Midstreet – sisarustrio</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
 </head>
+
 <body>
-    <?php include 'header.php'?>
+    <?php include 'header.php' ?>
+    <section>
+        <div>
+            <img src="/images/vince.png" alt="Vinsentti Koivula">
+            <img src="/images/vertti.png" alt="Vertti Koivula">
+            <img src="/images/viia.png" alt="Viia Koivula">
+            <img src="" alt="">
+        </div>
+    </section>
 </body>
+
 </html>
