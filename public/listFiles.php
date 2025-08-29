47b4335b0490508390b12fb7e46f63ac094700fc
Vertti Koivula <vertti.koivula@gmail.com>
2025-08-29 15:34:49 +0300

    Updated security in env and .gitignore

--- public/listFiles.php
+++ public/listFiles.php
@@ -8,12 +8,12 @@ use Google\Service\Drive;
 
 // Load credentials
 $client = new Client();
-$client->setAuthConfig(__DIR__ . '/../config/midstreetdb-2c993e5c7ef1.json'); // Credential for authenticating the script
+$client->setAuthConfig($_ENV['GOOGLE_APPLICATION_CREDENTIALS']); // Credential for authenticating the script
 $client->addScope(Drive::DRIVE_READONLY); // Only read access, not to modify or delete anything
 
 $service = new Drive($client); // The service that "talks" with G Drive
 
-$folderId = '0B3_SOi8p4Q6wX0VqYzlCd01PVEU';
+$folderId = '0B3_SOi8p4Q6wX0VqYzlCd01PVEU'; // The drive ID folder
 
 // Query to folder ID for data:
 
@@ -21,7 +21,7 @@ $optParams = [
     'q' => "'$folderId' in parents and trashed = false", // Show me files inside this folder and not in temporary trashcan
     'fields' => 'files(name)', // Only query three properties for each file, not all metadata
     'pageSize' => 300,
-    'orderBy' => 'name'  // Add this line
+    'orderBy' => 'name' // Show in alphabetical order
 ];
 
 $response = $service->files->listFiles($optParams);
